12f4d891073fc827294a992b43770506d398845c
hellohasnain <hasnainraza1059@gmail.com>
2025-01-11 12:14:42 +0530

    create page.js for edit profile folder

---
+++ components/Editprofile/Page.js
@@ -0,0 +1,13 @@
+import PersonalInfo from "./PersonalInfo";
+
+
+const Editprofile = () => {
+    return (
+        <div>
+            <PersonalInfo />
+
+        </div>
+    )
+}
+
+export default Editprofile;
