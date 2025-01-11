c128be43b1c26681b5db1a6329339bf732618e7f
Mo Rehan Chanija <120302087+rehanchanija@users.noreply.github.com>
2025-01-11 14:01:21 +0530

    app slider

--- components/AppSlider.js
+++ components/AppSlider.js
@@ -1,11 +1,32 @@
+import { useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-const AppSlider = ({ children, settings }) => {
+const AppSlider = ({ children, settings, nextSlide, prevSlide }) => {
+  const sliderRef = useRef(null);
+
+  const handleNextSlide = () => {
+    if (nextSlide) {
+      nextSlide();
+    } else {
+      sliderRef.current.slickNext();
+    }
+  };
+
+  const handlePrevSlide = () => {
+    if (prevSlide) {
+      prevSlide();
+    } else {
+      sliderRef.current.slickPrev();
+    }
+  };
+
   return (
-    <div>
-      <Slider {...settings}>{children}</Slider>
+    <div className="relative">
+      <Slider ref={sliderRef} {...settings}>
+        {children}
+      </Slider>
     </div>
   );
 };
