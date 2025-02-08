4e00d586e633f6d6089c6c3f09c5d084a8545443
hellohasnain <hasnainraza1059@gmail.com>
2025-02-08 15:28:04 +0530

    new api's

--- api/tutor.api.js
+++ api/tutor.api.js
@@ -10,7 +10,7 @@ export const updateTutor = async (values) => {
     const res = await axios.patch(`${API_URL}/tutor/update/${id}`, values)
     return res.data
 }
- export const getTutor = async () => {
+export const getTutor = async () => {
     const tutor = JSON.parse(localStorage.getItem("tutor"))
     const id = tutor?._id
     console.log(id)
@@ -18,4 +18,75 @@ export const updateTutor = async (values) => {
     return res.data
 
 
+}
+
+export const updateTutorEducationInfo = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const id = tutor?._id
+
+    const res = await axios.post(`${API_URL}/tutor/update/educationInfo/${id}`, values)
+    return res.data
+}
+
+export const updateTutorSubject = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const id = tutor?._id
+
+    const res = await axios.post(`${API_URL}/tutor/update/subject/${id}`, values)
+    return res.data
+}
+export const updateTutorExperience = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const id = tutor?._id
+
+    const res = await axios.post(`${API_URL}/tutor/update/experience/${id}`, values)
+    return res.data
+}
+
+export const updateTutorCourse = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const id = tutor?._id
+
+    const res = await axios.post(`${API_URL}/tutor/update/course/${id}`, values)
+    return res.data
+}
+
+export const deleteTutorExperience = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const tutorId = tutor?._id
+
+    const res = await axios.delete(`${API_URL}/tutor/delete/experience/${tutorId}/${values.id}`)
+    return res.data
+}
+
+export const deleteTutorCourse = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const tutorId = tutor?._id
+
+    const res = await axios.delete(`${API_URL}/tutor/delete/course/${tutorId}/${values.id}`)
+    return res.data
+}
+
+export const deleteTutorSubject = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const tutorId = tutor?._id
+
+    const res = await axios.delete(`${API_URL}/tutor/delete/subject/${tutorId}/${values.id}`)
+    return res.data
+}
+
+export const deleteTutorEducationInfo = async (values) => {
+    const tutor = JSON.parse(localStorage.getItem("tutor"))
+    console.log(tutor)
+    const tutorId = tutor?._id
+
+    const res = await axios.delete(`${API_URL}/tutor/delete/educationInfo/${tutorId}/${values.id}`)
+    return res.data
 }
