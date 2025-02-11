e65605483cd0b52472fe9b7cc9062925efcc43b4
hellohasnain <hasnainraza1059@gmail.com>
2025-02-11 20:04:04 +0530

    add button css

--- components/Editprofile/Course.js
+++ components/Editprofile/Course.js
@@ -305,15 +305,15 @@ const Course = ({ handlePrevious, handleNext, updateFormData, handleSubmit, init
                                         <button
                                             type="button"
                                             onClick={() => dirty ? handleCancel(resetForm, setValues) : handlePrevious()}
-                                            className="w-40 h-12 border border-gray-700 text-gray-700 font-bold rounded-md"
+                                            className="bg-transparent border border-[#0F283C] text-[#0F283C] py-2 md:px-7 px-4 rounded-md font-bold"
                                         >
-                                            {dirty ? "Cancel" : "&lt;&lt; Previous"}
+                                            {dirty ? "<< Cancel" : "<< Previous"}
                                         </button>
                                         <button
                                             type="submit"
-                                            className="w-40 h-12 bg-[#136AAD] text-white font-bold rounded-md"
+                                            className="bg-[#0F283C] text-white py-2 md:py-3 px-6 md:px-10 rounded text-sm md:text-lg font-semibold"
                                         >
-                                            {dirty ? "Save" : "Next >>"}
+                                            {dirty ? "Save >>" : "Next >>"}
                                         </button>
                                     </div>
                                 </Form>
